580d11a12177e1c2108d07fd39028d0d046a75e2
Reinger <dvbolotov@edu.hse.ru>
2018-01-15 10:51:19 +0500

    Ввод целого числа

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -8,8 +8,36 @@ namespace Task5
 {
     class Program
     {
+        static int ReadInt()
+        {
+            int k;
+            bool ok;
+
+            do
+            {
+                Console.Write("n=");
+                ok = Int32.TryParse(Console.ReadLine(), out k);
+                if (!ok)
+                {
+                    Console.WriteLine("Ошибка ввода. Нужно ввести целое число");
+                    Console.ReadLine();
+                    Console.Clear();
+                }
+                else if (k < 2)
+                {
+                    Console.WriteLine("Ошибка ввода. Нужно ввести целое число >=2");
+                    Console.ReadLine();
+                    Console.Clear();
+                    ok = false;
+                }
+            } while (!ok);
+
+            return k;
+        }
+
         static void Main(string[] args)
         {
+            int n = ReadInt();
         }
     }
 }
