1d431447790830ebf1f4bc64b828cef3109adfa1
Reinger <dvbolotov@edu.hse.ru>
2018-01-16 13:35:14 +0500

    Ввод и вывод массива

--- Task5/Program.cs
+++ Task5/Program.cs
@@ -15,7 +15,7 @@ namespace Task5
 
             do
             {
-                Console.Write("{0}",s);
+                Console.Write("{0}", s);
                 ok = Int32.TryParse(Console.ReadLine(), out k);
                 if (!ok)
                 {
@@ -28,9 +28,62 @@ namespace Task5
             return k;
         }
 
+        static int[,] VvDvumMasRan(int n)
+        {
+            int[,] b = new int[n, n];
+            int i, j;
+            Random r = new Random();
+            for (i = 0; i < n; i++)
+            for (j = 0; j < n; j++) b[i, j] = r.Next(11);
+            return b;
+        }
+
+        static int[,] VvDvumMasVr(int n)
+        {
+            int i, j = 0, i1 = 0, j1 = 0, i2 = 0, k;
+            int[,] b = new int[n, n];
+
+            for (k = 1; k <= n * n; k++)
+            {
+                Console.Clear();
+                Console.WriteLine("Массив: ");
+                for (i = 0; i < n; i++)
+                {
+                    for (j = 0; j < n; j++) if (i < i2 || i == i2 && j < j1) Console.Write("{0} ", b[i, j]);
+                        else if (i == i2 && j == j1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.Write("0 ");
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else Console.Write("0 ");
+                    Console.WriteLine();
+                }
+                b[i1, j1] = ReadInt("Введите целое число: ");
+                if (j1 < n - 1) j1++;
+                else
+                {
+                    i1++;
+                    j1 = 0;
+                    i2++;
+                }
+            }
+            return b;
+        }
+
+        static void OutMas(int[,] b, int n)
+        {
+            int i, j;
+            for (i = 0; i < n; i++)
+            {
+                for (j = 0; j < n; j++) Console.Write("{0} ", b[i, j]);
+                Console.WriteLine();
+            }
+        }
+
         static void Main(string[] args)
         {
-            int n = ReadInt("n=");
+            int n = ReadInt("n="), v;
             while (n < 2)
             {
                 Console.WriteLine("Ошибка ввода. n должно быть >=2");
@@ -43,12 +96,23 @@ namespace Task5
 
             int[,] m = new int[n, n];
 
-            for (int i = 0; i < n; i++)
-            for (int j = 0; j < n; j++)
+            do
             {
-                m[i, j] = ReadInt("m["+i+","+j+"]=");
+                v = ReadInt("Массив заполнить автоматически?\n1. Да\n2. Нет\nВвод: ");
+                if (v < 1 || v > 2)
+                {
+                    Console.WriteLine("Ошибка ввода. Ожидался ввод 1 или 2. Нажмите любуй кнопку и повторите ввод.");
+                    Console.ReadLine();
+                }
                 Console.Clear();
-            }
+            } while (v < 1 || v > 2);
+            if (v == 1) m = VvDvumMasRan(n);
+            else m = VvDvumMasVr(n);
+            Console.Clear();
+
+            OutMas(m,n);
+
+            Console.ReadLine();
         }
     }
 }
